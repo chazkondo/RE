1c636673b1bc984bebd4dbb772b5f3f6de41f0a0
Chaz Kondo <chazkondo@gmail.com>
2020-12-04 11:23:04 -1000

    Initial

---
+++ components/navigation.jsx
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
+const Navigation = (props) => {
+  const { island, selectIsland } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div style={{ backgroundColor: 'red' }}>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand style={{ paddingLeft: '20px' }} href="/">
+          xHawaii Homes
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav style={{ paddingRight: '30px' }} className="ml-auto" navbar>
+            <UncontrolledDropdown style={{ paddingRight: '30px' }} nav inNavbar>
+              <DropdownToggle data-placement="bottom" nav caret>
+                Real Estate
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem onClick={() => selectIsland('Kauai')}>
+                  Kauai
+                </DropdownItem>
+                <DropdownItem onClick={() => selectIsland('Molokai')}>
+                  Molokai
+                </DropdownItem>
+                <DropdownItem onClick={() => selectIsland('Oahu')}>
+                  Oahu
+                </DropdownItem>
+                <DropdownItem onClick={() => selectIsland('Lanai')}>
+                  Lanai
+                </DropdownItem>
+                <DropdownItem onClick={() => selectIsland('Maui')}>
+                  Maui
+                </DropdownItem>
+                <DropdownItem onClick={() => selectIsland('Big Island')}>
+                  Big Island
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <UncontrolledDropdown style={{ paddingRight: '30px' }} nav inNavbar>
+              <DropdownToggle data-placement="bottom" nav caret>
+                Rentals
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem onClick={() => selectIsland('Vacation Rentals')}>
+                  Vacation Rentals
+                </DropdownItem>
+                <DropdownItem onClick={() => selectIsland('Long Term Rentals')}>
+                  Long Term Rentals
+                </DropdownItem>
+                <DropdownItem
+                  onClick={() => selectIsland('Property Management')}
+                >
+                  Property Management
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem style={{ paddingRight: '30px' }}>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                News
+              </NavLink>
+            </NavItem>
+            <NavItem style={{ paddingRight: '30px' }}>
+              <NavLink onClick={() => selectIsland('Kauai')}>Login</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,121 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import 'antd/dist/antd.css';
+import { List, Space } from 'antd';
+import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
+import axios from 'axios';
+import Navigation from '@/components/navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Link from 'next/link';
+
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://ant.design',
+//     title: `ant design part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//       'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//       'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require('firebase/app');
+
+// Add the Firebase products that you want to use
+require('firebase/auth');
+require('firebase/firestore');
+
+const IconText = ({ icon, text }) => (
+  <Space>
+    {React.createElement(icon)}
+    {text}
+  </Space>
+);
+
+function mapReducer(state, newState) {
+  return newState;
+}
 
 export default function Home() {
+  const [listData, setListData] = React.useState([]);
+  const [itemPerPage, setItemPerPage] = React.useState(10);
+
+  const [island, selectIsland] = React.useReducer(mapReducer, null);
+
+  React.useEffect(() => {
+    axios
+      .get(
+        'https://api.apify.com/v2/acts/petr_cermak~zillow-api-scraper/runs/last/dataset/items?token=93P4WX9SLPyyLoghYYNrEq4bk'
+      )
+      .then(function (response) {
+        setListData(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <>
+      <Navigation island={island} selectIsland={selectIsland} />
+      <div style={{ paddingBottom: '20px' }}>
+        {island && (
+          <List
+            itemLayout="vertical"
+            size="large"
+            pagination={{
+              onChange: (page, pageSize) => {
+                setItemPerPage(pageSize);
+              },
+              pageSize: itemPerPage,
+            }}
+            dataSource={listData}
+            renderItem={(item) => (
+              <List.Item
+                key={item.address.streetAddress}
+                actions={[
+                  <IconText
+                    icon={StarOutlined}
+                    text="156"
+                    key="list-vertical-star-o"
+                  />,
+                  <IconText
+                    icon={LikeOutlined}
+                    text="156"
+                    key="list-vertical-like-o"
+                  />,
+                  <IconText
+                    icon={MessageOutlined}
+                    text="2"
+                    key="list-vertical-message"
+                  />,
+                ]}
+                extra={
+                  <img
+                    width={272}
+                    alt="logo"
+                    src={item.photos[item.photos.length - 1]}
+                  />
+                }
+              >
+                <List.Item.Meta
+                  title={
+                    <a href={'https://ant.design'}>
+                      {item.address.streetAddress}
+                    </a>
+                  }
+                  description={item.city}
+                />
+                {item.description}
+              </List.Item>
+            )}
+          />
+        )}
+      </div>
+    </>
+  );
 }
